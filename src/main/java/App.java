77659925159ea2f9aa001f60492d71a87f16a398
siwei <1138015321@qq.com>
2017-10-14 11:42:23 +0800

    make some change

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,14 @@
 /*
- * Alipay.com Inc.
+ * SiweiZhong
  * Copyright (c) 2004-2017 All Rights Reserved.
  */
 
 /**
  *
- * @author ${yifan}
- * @version $Id: App.java, v 0.1 2017年10月14日 上午11:36 ${yifan} Exp $
+ * @author ${SiweiZhong}
+ * @version $Id: App.java, v 0.1 2017年10月14日 上午11:41 ${SiweiZhong} Exp $
  */
 public class App {
-
     public static void main(String[] args) {
         System.out.println("hello world");
     }
